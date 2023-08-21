77a640719ece77bc43031432fad47a483f0f5ab9
vedarthjoshi <joshivedarth@gmail.com>
2023-08-21 16:34:42 +0530

    contect signup Vedarth abhay

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,35 @@
-let open = false;
+// let open = false;
 
-let x = document.querySelectorAll(".link-size");
-var i;
-for(i =0; i<5;i++){
-    if(x[i].addEventListener)
-    console.log(x[i]+" "+i)
-    if(x[i].addEventListener("click", haha())){
-        changeBg(i);
-    }
-}
-
-// if(x[0].addEventListener('click',(e) => {
-//     console.log('A link was clicked');
-//   })){
-//     console.log("haha")
-//     changeBg();
-// }else{
-//     console.log("nope")
+// let x = document.querySelectorAll(".link-size");
+// var i;
+// for(i =0; i<5;i++){
+//     if(x[i].addEventListener)
+//     console.log(x[i]+" "+i)
+//     if(x[i].addEventListener("click", haha())){
+//         changeBg(i);
+//     }
 // }
 
-function changeBg(i){
-    console.log("Changed")
-    x[i].style.background = "linear-gradient(-90deg, rgba(0, 159, 119, 0.819),rgba(60, 60, 255, 0.819)), rgba(60, 60, 255, 0.719)";
-}
+// // if(x[0].addEventListener('click',(e) => {
+// //     console.log('A link was clicked');
+// //   })){
+// //     console.log("haha")
+// //     changeBg();
+// // }else{
+// //     console.log("nope")
+// // }
 
-function haha(){
-    console.log("button check")
-}
+// function changeBg(i){
+//     console.log("Changed")
+//     x[i].style.background = "linear-gradient(-90deg, rgba(0, 159, 119, 0.819),rgba(60, 60, 255, 0.819)), rgba(60, 60, 255, 0.719)";
+// }
+
+// function haha(){
+//     console.log("button check")
+// }
+
+// $(document).ready(function(){
+//     $(".each-link").click(function(){
+//       $("h1").hide();
+//     });
+//   });
